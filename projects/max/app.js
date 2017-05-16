d693beebb49949f33dfe89f3f4b5d77db967e1a4
AlexEarleyCrossmatch <alex.earley@crossmatch.com>
2017-05-16 19:45:30 -0400

    added gallery functionality to max's site

--- projects/max/app.js
+++ projects/max/app.js
@@ -11,9 +11,7 @@ function AppController($scope, $rootScope, $mdDialog, $timeout, $window) {
     vm.Images = staticImageList;
     vm.Columns = 3;
     vm.Radius = 5;
-    $timeout(function () { vm.Columns = 4; }, 0);
-   
-
+    $timeout(function () { vm.Columns = 4; }, 0); 
 });
 
 angular.module('GalleryAwesomeApp').directive('galleryAwesome', function ($timeout, $rootScope, $parse) {
@@ -38,29 +36,21 @@ angular.module('GalleryAwesomeApp').directive('galleryAwesome', function ($timeo
             function updateThumbs(size)
             {
                 var newColWidth = parseInt((80 / parseInt(size))) - 2;
-                var newStyle = "height:" + newColWidth + "vw;width:" + newColWidth + "vw;"; //+
-                 //   "clip-path:clip-path: circle(" + $scope.radius + "vw at 50% 50%);" +
-     //"-webkit-clip-path: circle(" + $scope.radius + "vw at  50% 50%);";
+                var newStyle = "height:" + newColWidth + "vw;width:" + newColWidth + "vw;"; 
                 angular.forEach(document.getElementsByClassName('gallery-awesome-thumb-container'), function (elem) {
                     elem.setAttribute("style", newStyle);
                 });
                 angular.forEach(document.getElementsByClassName('gallery-awesome-thumb'), function (elem) {
                     elem.setAttribute("style", newStyle);
-                });
-                //var newContainerStyle = "height:" + newColWidth + "vw;width:" + newColWidth + "vw;";
-                //angular.forEach(document.getElementsByClassName('gallery-awesome-thumb-container'), function (elem) {
-                //    elem.setAttribute("style", newContainerStyle);
-                //});
+                });                
             }
-            //setTimeout(updateThumbs(3),10000);
-
         },
+
         controller: function ($scope, $element, $mdDialog) {
             var vm = this;
             vm.Images = $scope.images;           
             vm.ShowFull = function (ev,img) {
-                $mdDialog.show({
-                    
+                $mdDialog.show({                    
                     controller: GalleryAwesomeDialogController,
                     controllerAs:"vm",
                     templateUrl: 'gallery-awesome-dialog.html',
@@ -70,19 +60,52 @@ angular.module('GalleryAwesomeApp').directive('galleryAwesome', function ($timeo
                     fullscreen: true,
                     locals : {
                         imageUrl: img.full,
+                        gallery:img.gallery,
                         caption:img.caption
                 }
                 });
                    
             };
 
-            function GalleryAwesomeDialogController($scope, $mdDialog, imageUrl,caption) {
+            function GalleryAwesomeDialogController($scope, $mdDialog, imageUrl, gallery,caption) {
                 var vm = this;
                 vm.Close = function () {
                     $mdDialog.hide();
                 };
+                vm.Prev = function () {
+                    vm.Reset();
+                    vm.CurrentIndex--;
+                    if (vm.CurrentIndex <= 0)
+                    {
+                        vm.ShowPrev = false;
+                        vm.CurrentIndex = 0;
+                    }
+                      
+                };
+
+                vm.Next = function () {
+                    vm.Reset();
+                    vm.CurrentIndex++;
+                    if (vm.CurrentIndex >= vm.Gallery.length-1)
+                    {
+                        vm.ShowNext = false;
+                        vm.CurrentIndex = vm.Gallery.length - 1;
+                    }
+                };
+
+                vm.Reset=function()
+                {
+                    vm.ShowNext = true;
+                    vm.ShowPrev = true;
+                }
+
                 vm.Image = imageUrl;
+                vm.Gallery = gallery;
+                vm.IsGallery = vm.Gallery != undefined && vm.Gallery.length > 0;
+                vm.ShowPrev = false;
+                vm.ShowNext = vm.IsGallery;
                 vm.Caption = caption;
+                vm.CurrentIndex = 0;
             };
 
 
@@ -91,12 +114,10 @@ angular.module('GalleryAwesomeApp').directive('galleryAwesome', function ($timeo
     };
 });
 
-
 var staticImageList = [{ full: 'images/design-jobby.png', thumb: 'thumbs/design-jobby_t.png', caption: 'Jobby Pro - Web Design', style: 'circle' },
     { full: 'images/design-rate-scout.png', thumb: 'thumbs/design-rate-scout_t.png', caption: 'Rate Scout - Web Design', style: 'circle' },
-    { full: 'images/design-solar1.png', thumb: 'thumbs/design-solar_t.png', caption: 'American Solar - Web Design - 1', style: 'circle' },
-    { full: 'images/design-solar2.png', thumb: 'thumbs/design-solar_t.png', caption: 'American Solar - Web Design - 2', style: 'circle' },
-    { full: 'images/hands.png', thumb: 'thumbs/hands_t.png', caption: 'Hands - Woodblock Print - 1', style: 'cross' },
-    { full: 'images/hands2.png', thumb: 'thumbs/hands_t.png', caption: 'Hands - Woodblock Print - 2', style: 'cross' }
+    { gallery: [{ full: 'images/design-solar1.png', index: 0 }, { full: 'images/design-solar2.png', index:1}], thumb: 'thumbs/design-solar_t.png', caption: 'American Solar - Web Design', style: 'circle' },
+    { gallery: [{ full: 'images/hands.png', index: 0 }, { full: 'images/hands2.png', index: 1}], thumb: 'thumbs/hands_t.png', caption: 'Hands - Woodblock Print', style: 'cross' }
         
 ];
+
